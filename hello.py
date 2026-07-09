08405332710aba26ed0c7ab07745ff055c93cdda
Darlingchirinos5 <301115108+Darlingchirinos5@users.noreply.github.com>
2026-07-09 22:07:30 +0000

    Complete Lab 1

--- hello.py
+++ hello.py
@@ -4,4 +4,4 @@ def greet(name):
 
 if __name__ == "__main__":
  student_name = input("What's your name? ")
-    print(greet(student_name))
+print(greet(student_name))
--- labwork/lab01-input-output/main.py
+++ labwork/lab01-input-output/main.py
@@ -4,7 +4,7 @@
 
 # Start writing your code below.
 # print("Hello World")
-#  the "Run" button (or run this file in the terminal with: python main.py)
+# myname=input ("What's your name") 
 # to see your output in the terminal window.
 # Good luck! You can do this - please don't use any AI tools to do this assignment. 
 # That violates the AI Policy for this class and puts your learning at a disadvantage. 
